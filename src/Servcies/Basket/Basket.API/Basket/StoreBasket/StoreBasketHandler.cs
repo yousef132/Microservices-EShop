d2b57d19add628f93fcf59f63f84eb4fbf64a439
youssefsaad-spec <youssef.saad@tildetech.ae>
2025-12-09 12:27:01 +0200

    consume discount service from basket service via gRPC communication

--- src/Servcies/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Servcies/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Basket.API.Basket.StoreBasket;
+﻿using Discount.gRPC;
+
+namespace Basket.API.Basket.StoreBasket;
 
 public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
 public record StoreBasketResult(string UserName);
@@ -13,25 +15,25 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
 }
 
 public class StoreBasketCommandHandler
-    (IBasketRepository repository /*DiscountProtoService.DiscountProtoServiceClient discountProto*/)
+    (IBasketRepository repository ,DiscountProtoService.DiscountProtoServiceClient discountProto)
     : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
-        //await DeductDiscount(command.Cart, cancellationToken);
+       await DeductDiscount(command.Cart, cancellationToken);
 
         await repository.StoreBasket(command.Cart, cancellationToken);
 
         return new StoreBasketResult(command.Cart.UserName);
     }
 
-    //private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
-    //{
-    //    // Communicate with Discount.Grpc and calculate lastest prices of products into sc
-    //    foreach (var item in cart.Items)
-    //    {
-    //        var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
-    //        item.Price -= coupon.Amount;
-    //    }
-    //}
+    private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        // Communicate with Discount.Grpc and calculate lastest prices of products into sc
+        foreach (var item in cart.Items)
+        {
+            var coupon = await discountProto.GetDiscountAsync(new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+            item.Price -= coupon.Amount;
+        }
+    }
 }
